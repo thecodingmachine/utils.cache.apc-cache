771f0e957ef1b5c5c049cf8c053c76045b794147
camklcbr <cam.koppel@gmail.com>
2015-01-14 16:48:35 +0100

    Add new installer for package

---
+++ src/Mouf/Utils/Cache/ApcCacheInstaller.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright (c) 2013-2015 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+namespace Mouf\Utils\Cache;
+
+use Mouf\Installer\PackageInstallerInterface;
+use Mouf\MoufManager;
+
+/**
+ * An installer class for APC Cache
+ */
+class ApcCacheInstaller implements PackageInstallerInterface {
+
+    /**
+     * (non-PHPdoc)
+     * @see \Mouf\Installer\PackageInstallerInterface::install()
+     * @param MoufManager $moufManager
+     * @throws \Mouf\MoufException
+     */
+    public static function install(MoufManager $moufManager) {
+        if (!$moufManager->instanceExists("apcCacheService")) {
+            $apcCacheService = $moufManager->createInstance("Mouf\\Utils\\Cache\\ApcCache");
+            $apcCacheService->setName("apcCacheService");
+            $apcCacheService->getProperty("defaultTimeToLive")->setValue(3600);
+            $apcCacheService->getProperty("prefix")->setValue('SECRET')->setOrigin('config');
+
+            /*if ($moufManager->instanceExists("errorLogLogger")) {
+                $apcCacheService->getProperty("log")->setValue($moufManager->getInstanceDescriptor("errorLogLogger"));
+            }*/
+
+            if ($moufManager->instanceExists("fileCacheService")) {
+                $apcCacheService->getProperty("fallback")->setValue($moufManager->getInstanceDescriptor("fileCacheService"));
+            }
+        } else {
+            $apcCacheService = $moufManager->getInstanceDescriptor("apcCacheService");
+        }
+
+        $configManager = $moufManager->getConfigManager();
+        $constants = $configManager->getMergedConstants();
+        if (isset($constants['ROOT_URL'])) {
+            $apcCacheService->getProperty('prefix')->setValue('ROOT_URL')->setOrigin('config');
+        }
+
+        // Let's rewrite the MoufComponents.php file to save the component
+        $moufManager->rewriteMouf();
+
+    }
+}
